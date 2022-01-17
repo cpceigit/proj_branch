383a36137e1bf71ab9e08c46b6355ea8aa27e60a
s.fujimoto <s.fujimoto@chugoku-pc.local>
2022-01-17 15:32:09 +0900

    [change] directory and file

---
+++ cgitest/cgitest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Content-type:text/html\n\n");
+    printf("<html>\n");
+    printf("<head>\n");
+    printf("<title>\n");
+    printf("cgi-sample\n");
+    printf("</title>\n");
+    printf("</head>\n");
+    printf("<body>\n");
+    printf("<h1>cgitest.cgi!!!!!!!!!!!</h1>\n");
+    printf("</body>\n");
+    printf("</html>\n");
+    return 0;
+}
