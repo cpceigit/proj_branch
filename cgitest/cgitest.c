98aeb9659dc6d0331262b512f78dc0217787077f
elmore200git <s2.fujimoto.git@gmail.com>
2022-01-17 19:43:14 +0900

    [change] file

--- cgitest/cgitest.c
+++ cgitest/cgitest.c
@@ -5,11 +5,11 @@ int main(void){
     printf("<html>\n");
     printf("<head>\n");
     printf("<title>\n");
-    printf("cgi-sample\n");
+    printf("Git test branch \n");
     printf("</title>\n");
     printf("</head>\n");
     printf("<body>\n");
-    printf("<h1>cgitest.cgi!!!!!!!!!!!</h1>\n");
+    printf("<h1>cgitest.cgi from git test branch.</h1>\n");
     printf("</body>\n");
     printf("</html>\n");
     return 0;
