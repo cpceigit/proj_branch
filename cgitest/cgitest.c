5c7ec83cafc29adc8fd5507d1b2a1df1a312e039
elmore200git <s2.fujimoto.git@gmail.com>
2022-01-22 18:14:03 +0900

    [change] file

--- cgitest/cgitest.c
+++ cgitest/cgitest.c
@@ -9,7 +9,7 @@ int main(void){
     printf("</title>\n");
     printf("</head>\n");
     printf("<body>\n");
-    printf("<h1>cgitest.cgi from git branch both master and test</h1>\n");
+    printf("<h1>cgitest.cgi from git branch both local and remote</h1>\n");
     printf("</body>\n");
     printf("</html>\n");
     return 0;
